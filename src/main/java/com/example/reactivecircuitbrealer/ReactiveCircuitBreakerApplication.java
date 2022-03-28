e45753ed8289d5517db1f434bf375c0ca825cfca
Kelig MARTIN <kmartin@wedigital.garden>
2022-03-29 00:54:10 +0200

    initial commit

---
+++ src/main/java/com/example/reactivecircuitbrealer/ReactiveCircuitBreakerApplication.java
@@ -0,0 +1,16 @@
+package com.example.reactivecircuitbrealer;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class ReactiveCircuitBreakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveCircuitBreakerApplication.class, args);
+
+    }
+}
